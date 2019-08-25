08cabbfb15c2d01b504645486b80d3e2e10b22f2
mbabalghaith <mbabalghaith@gmail.com>
2019-08-25 21:04:36 +0300

    Day6

---
+++ venv/Day6.py
@@ -0,0 +1,31 @@
+x = int(3)
+y = int(4.6)
+z = int('1')
+print ('convert to int ')
+print(x)
+print (y)
+print (z)
+
+print ('-----------------------')
+x = float(3)
+y = float(6.2)
+z = float('2')
+
+print ('convert to float')
+print(x)
+print (y)
+print (z)
+
+print ('-----------------------')
+
+x = str('S1')
+y = str(2)
+z = str(3.0)
+
+print ('convert to string')
+
+print(x)
+print (y)
+print (z)
+
+
