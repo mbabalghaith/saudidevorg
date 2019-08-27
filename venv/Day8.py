0a354c412d003275cca2835b0f5c936ce730080a
mbabalghaith <mbabalghaith@gmail.com>
2019-08-28 01:53:05 +0300

    Day8

---
+++ venv/Day8.py
@@ -0,0 +1,25 @@
+a = " My Name Is Mohammed "
+
+print(a.strip())
+
+print  ("---------------------")
+
+
+
+print ("Number of characters " , len(a))
+
+print  ("---------------------")
+
+print ("lower case " , a.lower())
+
+print  ("---------------------")
+
+print (" Upper case " , a.upper())
+
+print  ("---------------------")
+
+print (a.replace("N" , "S"))
+
+print  ("---------------------")
+
+print (a.split(" , "))
