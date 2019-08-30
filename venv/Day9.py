1ff83f8adc3cde0f5622328c06e5cea3babe8b7b
mbabalghaith <mbabalghaith@gmail.com>
2019-08-30 03:20:51 +0300

    Day009

---
+++ venv/Day9.py
@@ -0,0 +1,14 @@
+age = 57
+
+txt = "My name is Jhon, I am {}"
+print (txt.format(age))
+
+print ("------------------------")
+
+quantity = 9
+itemno = 4
+price = 34.89
+
+myorder = " I want {} pieces of item {} for {} dollars. "
+
+print (myorder.format(itemno, quantity, price))
