5d9b682c737fac2230b7230b627ff3df3d94d11d
mbabalghaith <mbabalghaith@gmail.com>
2019-09-17 02:36:45 +0300

    Day28

---
+++ venv/Day28.py
@@ -0,0 +1,21 @@
+i = 0
+f = True
+while i < 4:
+    print (i)
+    i += 1
+print ('---------------')
+
+while i <= 4:
+    print (i)
+    i += 1
+else:
+    print ('i greater than 4')
+
+print ('--------------------------')
+m = 0
+
+while f:
+    print (m)
+    m +=1
+    if m > 4:
+        f = False
