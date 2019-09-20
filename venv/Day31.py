dbdaa66e95b3f7326aeb247b1ce31618600f0e59
mbabalghaith <mbabalghaith@gmail.com>
2019-09-20 08:26:10 +0300

    Day31

---
+++ venv/Day31.py
@@ -0,0 +1,22 @@
+def function1():
+    print ('Hello world')
+    print ('----------------------')
+
+
+function1()
+
+def function2(fname):
+    print ('Hello '+fname)
+    print ("----------------")
+
+
+
+function2('Mohammed')
+
+def function3(city = 'Ryiadh'):
+    print ('I am from ' + city)
+
+
+function3('Makkah')
+function3('Jeddah')
+function3()
