39eb9efdc8c4ef0c92fef04d1d7bf19a3f789555
mbabalghaith <mbabalghaith@gmail.com>
2019-09-28 00:56:06 +0300

    Day37&38

---
+++ venv/Day37&38.py
@@ -0,0 +1,29 @@
+a = ['Mohammed','ahmed','ali','khalid']
+
+print (a)
+
+a[0]= 'nasser'
+
+print (a)
+print (len(a))
+print ('-----------------')
+
+for x in a:
+    print (x)
+
+print ('-------------')
+
+a.append('Meshal')
+
+print (a)
+
+print ('-------------')
+
+a.pop(2)
+
+print (a)
+print ('------------')
+
+a.remove('nasser')
+
+print (a)
